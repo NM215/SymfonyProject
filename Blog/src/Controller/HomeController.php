082f2ac7343f708104e200570bb006d48f346bff
NM215 <nadia.medouaz@gmail.com>
2020-12-26 02:19:08 +0100

    CRUD controller

--- Blog/src/Controller/HomeController.php
+++ Blog/src/Controller/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
