016f9b074accff52c3999da88ea7f1f4bba96a5d
NM215 <nadia.medouaz@gmail.com>
2020-12-27 00:08:01 +0100

    update

--- Blog/src/Controller/APIRestPostController.php
+++ Blog/src/Controller/APIRestPostController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class APIRestPostController extends AbstractController
 {
@@ -23,7 +24,7 @@ class APIRestPostController extends AbstractController
     /**
      * @Route("/api/posts", name="api_posts",  methods={"GET"})
      */
-    public function getAllPosts(PostRepository $ripo) : JsonResponse
+    public function getMyPosts(PostRepository $ripo) : JsonResponse
     {
         $posts = $ripo->findLastFivePosts();
         $data = [];
@@ -39,5 +40,23 @@ class APIRestPostController extends AbstractController
         }
 
         return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/api/otherposts", name="api_posts_other")
+     */
+    public function getOtherPosts(HttpClientInterface $client) : Response
+    {
+        $response = $client->request(
+            'GET',
+            'https://kaloun-mouhali-blog.herokuapp.com/api/posts'
+        );
+
+        $content = $response->getContent();
+        $content = $response->toArray();
+
+        return $this->render('api_rest_post/rest.html.twig', [
+            'posts' => $content
+        ]);
     }
 }
